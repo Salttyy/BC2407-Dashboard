62d2a294f9e9dc67fdfa6a1b3d26751e08a83658
Saltykub <nathan.juir@gmail.com>
2025-03-01 17:56:13 +0800

    update: EDA

--- pages/1_Eda.py
+++ pages/1_Eda.py
@@ -2,39 +2,45 @@ import streamlit as st
 import numpy as np
 import plotly.express as px
 import pandas as pd
+import seaborn as sb
 
 df = pd.read_csv("telecom_churn.csv")
 
-st.markdown ("## Exploratory Data Analysis")
+st.markdown ("# Exploratory Data Analysis")
 st.markdown("### Summary of Telecom churn data")
 
 st.dataframe(df.describe())
 
-color = px.colors.qualitative.Dark24    
-
-df["Churn"] = df["Churn"].astype(str);
-
-fig1 = px.box(df, 
-              y="AccountWeeks", 
-              color="Churn", 
-              facet_col="Churn",
-              title="Account Weeks vs Churn",
-              color_discrete_sequence=color)
-
-fig1.update_xaxes(title_text="")  
-fig1.update_xaxes(showticklabels=False) 
-fig1.update_xaxes(showgrid=False)  
-
-st.plotly_chart(fig1)
-
-fig2 = px.histogram(df, 
-                    x="AccountWeeks", 
-                    color="Churn", 
-                    opacity=0.5, 
-                    barmode="overlay",
-                    title="Distribution of Account Weeks by Churn",
-                    labels={"Churn": "Churn Status"},
-                    color_discrete_sequence=color)
+st.markdown("## Overall Statistic")
 
 
-st.plotly_chart(fig2)
+
+
+# color = px.colors.qualitative.Dark24    
+
+# df["Churn"] = df["Churn"].astype(str);
+
+# fig1 = px.box(df, 
+#               y="AccountWeeks", 
+#               color="Churn", 
+#               facet_col="Churn",
+#               title="Account Weeks vs Churn",
+#               color_discrete_sequence=color)
+
+# fig1.update_xaxes(title_text="")  
+# fig1.update_xaxes(showticklabels=False) 
+# fig1.update_xaxes(showgrid=False)  
+
+# st.plotly_chart(fig1)
+
+# fig2 = px.histogram(df, 
+#                     x="AccountWeeks", 
+#                     color="Churn", 
+#                     opacity=0.5, 
+#                     barmode="overlay",
+#                     title="Distribution of Account Weeks by Churn",
+#                     labels={"Churn": "Churn Status"},
+#                     color_discrete_sequence=color)
+
+
+# st.plotly_chart(fig2)
