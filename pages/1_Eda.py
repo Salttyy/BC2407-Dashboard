f2ca5bc836c18c51450558a7e0b520e3b9c53a75
Saltykub <nathan.juir@gmail.com>
2025-03-03 15:30:51 +0800

    add: SMOTE

--- pages/1_Eda.py
+++ pages/1_Eda.py
@@ -16,31 +16,3 @@ st.markdown("## Overall Statistic")
 
 
 
-# color = px.colors.qualitative.Dark24    
-
-# df["Churn"] = df["Churn"].astype(str);
-
-# fig1 = px.box(df, 
-#               y="AccountWeeks", 
-#               color="Churn", 
-#               facet_col="Churn",
-#               title="Account Weeks vs Churn",
-#               color_discrete_sequence=color)
-
-# fig1.update_xaxes(title_text="")  
-# fig1.update_xaxes(showticklabels=False) 
-# fig1.update_xaxes(showgrid=False)  
-
-# st.plotly_chart(fig1)
-
-# fig2 = px.histogram(df, 
-#                     x="AccountWeeks", 
-#                     color="Churn", 
-#                     opacity=0.5, 
-#                     barmode="overlay",
-#                     title="Distribution of Account Weeks by Churn",
-#                     labels={"Churn": "Churn Status"},
-#                     color_discrete_sequence=color)
-
-
-# st.plotly_chart(fig2)
